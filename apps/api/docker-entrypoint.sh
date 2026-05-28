d93801825111cb107954a5012641cd6b68c27f0c
Bharat Raj Verma <bharat.raj1508@gmail.com>
2026-05-28 16:42:25 +0530

    setup migration on docker build

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Run database migrations in production
 echo "Running database migrations..."
-npx prisma migrate deploy --schema=./prisma/schema.prisma
+yarn prisma migrate deploy --schema=./prisma/schema.prisma
 
 echo "Starting application..."
 exec node dist/main.js
